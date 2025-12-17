1f820b11393721121ed54f7328ad8748cf55e281
dev <devonbush348@gmail.com>
2025-12-16 21:24:26 -0500

    Create dutyZones.lua

---
+++ dutyZones.lua
@@ -0,0 +1,10 @@
+ **mythic-businesses/client/dutyZones.lua**
+
+Polyzone.Create:Box('taco_offdutyzone', vector3(13.38, -1603.79, 29.39), 14.6, 13.0, {
+        heading = 320,
+        debugPoly=true,
+        minZ = 28.36,
+        maxZ = 33.16
+    }, {
+        goOffDuty = 'taco'
+    })
