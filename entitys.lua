64f503ca02421bc75bf28b0951551472dfb2357a
dev <devonbush348@gmail.com>
2025-12-16 21:23:11 -0500

    Create entitys.lua

---
+++ entitys.lua
@@ -0,0 +1,24 @@
+mythic-inventory/server/data/entitys.lua
+
+{
+		id = 139,
+		slots = 140,
+		capacity = 4000,
+		name = "Taco Farmer Freezer",
+		restriction = {
+			job = {
+				id = "taco",
+			},
+		},
+	},
+	{
+		id = 140,
+		slots = 52,
+		capacity = 500,
+		name = "Taco Farmer Food Warmer",
+		restriction = {
+			job = {
+				id = "taco",
+			},
+		},
+	},
